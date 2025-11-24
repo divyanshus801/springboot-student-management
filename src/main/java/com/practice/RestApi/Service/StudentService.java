82a64644a7b7634f2ec2fc351df68f698945de1c
divyanshus801 <divyanshus801@gmail.com>
2025-11-24 18:45:22 +0530

    added update , list all, delete api

--- src/main/java/com/practice/RestApi/Service/StudentService.java
+++ src/main/java/com/practice/RestApi/Service/StudentService.java
@@ -5,7 +5,9 @@ import com.practice.RestApi.Entity.Student;
 import com.practice.RestApi.Repository.StudentRepository;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -39,6 +41,31 @@ public class StudentService {
             studentDtoList.add(dto);
         }
         return studentDtoList;
+    }
+
+    public StudentDto updateStudent(Long id, StudentDto studentDto) {
+        Student existingStudent = studentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Student not found"));
+
+        existingStudent.setName(studentDto.getName());
+        existingStudent.setEmail(studentDto.getEmail());
+
+        Student updatedStudent = studentRepository.save(existingStudent);
+
+        return new StudentDto(updatedStudent.getId(), updatedStudent.getName(), updatedStudent.getEmail());
+    }
+
+    public Map<String, Object> deleteStudent(Long id) {
+
+        Student existingStudent = studentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Student not found"));
+
+        studentRepository.delete(existingStudent);
+
+         Map<String, Object> response = new HashMap<>();
+         response.put("message", "Student deleted successfully");
+         response.put("deletedStudent", existingStudent);
+        return response;
     }
 
 }
